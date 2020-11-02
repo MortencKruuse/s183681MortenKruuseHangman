d77b957922d660bb2ac597e2b8361fdec492b15c
MortencKruuse <s183681@student.dtu.dk>
2020-11-02 21:11:04 +0100

    Made some xml pages

--- app/src/main/java/com/example/s183681mortenkruusehangman/MainActivity.java
+++ app/src/main/java/com/example/s183681mortenkruusehangman/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.s183681mortenkruusehangman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button startbtn = findViewById(R.id.startButton);
+        startbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, hangmangame.class));
+            }
+        });
     }
 }
